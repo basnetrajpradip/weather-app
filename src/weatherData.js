e19749e36cc144167a0b0f714a558b838250355f
basnetrajpradip <basnetrajpradip@gmail.com>
2023-09-10 10:08:00 +0545

    Apply https

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,7 +1,7 @@
 // async function for requesting data to the api
 export default async function requestCurrentData(location) {
   const API_KEY = "6af34468f7a04780b5c60908232408";
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`;
 
   const response = await fetch(url, { mode: "cors" });
   if (!response.ok) {
